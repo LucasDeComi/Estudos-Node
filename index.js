ff848b6357093214f15d9391749a189a7e17a633
Lucas <lcorte110210@gmail.com>
2026-06-07 20:02:52 -0300

    Utilizando os métodos do módulo path

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 
 const { Person } = require("./person") // Obtém o Person do módulo person.js
 
+require("./modules/path"); // Utilizar o require sozinho somente executa os comandos no script
+
 const person = new Person("Lucas");
-console.log(person.sayMyName());
---
+++ modules/path.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+// Basename, método para retornar nome do arquivo
+console.log(path.basename(__filename)); // __filename é um valor com o nome do arquivo em que o código está sendo executado
+
+// Dirname, método para retornar diretório do arquivo
+console.log(path.dirname(__filename));
+
+// Extname, método para retornar extensão do arquivo
+console.log(path.extname(__filename))
+
+// Parse, método para criar objetos Path
+console.log(path.parse(__filename));
+
+// Join, método para juntar caminhos
+console.log(path.join(__dirname, "test", "test.js")) // __dirname é um valor com o caminho até o arquivo atual
