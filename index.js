5e6d8a85950bcd204397e3f549266523b4b65fff
Lucas <lcorte110210@gmail.com>
2026-06-15 16:07:30 -0300

    Criando um servidor com o módulo http

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const { Person } = require("./person") // Obtém o Person do módulo person.js
 
 // require("./modules/path"); // Utilizar o require sozinho somente executa os comandos no script
-require("./modules/fs");
+// require("./modules/fs");
+require("./modules/http");
 
 const person = new Person("Lucas");
